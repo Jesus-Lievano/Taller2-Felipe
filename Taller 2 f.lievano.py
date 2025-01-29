cd3b18eeaa0aa690fa17c96574bbe4a7e0356d1e
Jesus-Lievano <f.lievano@uniandes.edu.co>
2025-01-29 16:00:19 -0500

    Creación histograma para la distribución

--- Taller 2 f.lievano.py
+++ Taller 2 f.lievano.py
@@ -5,3 +5,11 @@ lamb = 5
 n = 1000
 
 vals = np.random.exponential(scale=1/lamb, size= n)
+
+import matplotlib.pyplot as plt
+
+count, bins, ignored = plt.hist(x=vals, bin = 30)
+plt.title("Histograma Distribución Exponencial")
+plt.xlabel("Tiempos Entre Arribos")
+plt.ylabel("Frecuencia")
+plt.show()
