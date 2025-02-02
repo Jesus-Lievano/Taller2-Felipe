77cae18f968b6fabce9e317fc89abffeb840d2e0
Marti820 <j.vasquezc@uniandes.edu.co>
2025-02-02 09:57:37 -0500

    commit martin (^_^)_/

--- Taller 2 f.lievano.py
+++ Taller 2 f.lievano.py
@@ -19,3 +19,27 @@ import scipy.stats as stats
 
 fig = sm.qqplot(data = vals, dist= stats.distributions.expon(scale = 1/lamb),line="45")
 plt.show()
+
+#-------mods-------martin---------
+
+import numpy as np
+#aqui ^_^
+lamb = 2
+
+n = 1000
+
+vals = np.random.exponential(scale=1/lamb, size= n)
+
+import matplotlib.pyplot as plt
+
+count, bins, ignored = plt.hist(x=vals, bin = 30)
+plt.title("Histograma Distribución Exponencial")
+plt.xlabel("Tiempos Entre Arribos")
+plt.ylabel("Frecuencia")
+plt.show()
+
+import statsmodels.api as sm
+import scipy.stats as stats
+
+fig = sm.qqplot(data = vals, dist= stats.distributions.expon(scale = 1/lamb),line="45")
+plt.show()
